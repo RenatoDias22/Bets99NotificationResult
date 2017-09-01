6c91fb46b4f01dcc24a0b6f02a8883ff0b439fa5
Renato Dias <lrds1722@gmail.com>
2017-09-01 19:03:39 -0300

    alterações no mobile

--- app/src/main/java/com/example/renatodias/bets99notificationresult/activitys/Splash.java
+++ app/src/main/java/com/example/renatodias/bets99notificationresult/activitys/Splash.java
@@ -1,23 +1,15 @@
 package com.example.renatodias.bets99notificationresult.activitys;
 
 import android.app.Activity;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.renatodias.bets99notificationresult.Global.GlobalJogo;
 import com.example.renatodias.bets99notificationresult.R;
-import com.example.renatodias.bets99notificationresult.adapter.JogosAdapter;
 import com.example.renatodias.bets99notificationresult.model.JogoResponse;
 import com.google.gson.Gson;
 
@@ -25,60 +17,34 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * Created by renatodias on 01/09/17.
+ */
 
-public class MainActivity extends AppCompatActivity {
+public class Splash extends Activity {
 
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    RecyclerView.Adapter adapter;
-    ProgressBar progress;
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        progress = (ProgressBar) findViewById(R.id.secondBar);
-//        progress.setVisibility(View.VISIBLE);
-//        this.request();
-//        try {
-//            Thread.sleep(4000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        this.Create();
-    }
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splashscreen);
+        new HttpAsyncTask().execute("http://api.bets99.com/index.php/apimobile/listNoResultGames");
 
-    private synchronized void request () {
-        synchronized (this) {
-            new HttpAsyncTask().execute("http://api.bets99.com/index.php/apimobile/listNoResultGames");
-        }
-    }
-
-    private synchronized void Create() {
-        recyclerView =
-                (RecyclerView) findViewById(R.id.recycler_view);
-
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        adapter = new JogosAdapter();
-        recyclerView.setAdapter(adapter);
-    }
-
-    public boolean isConnected(){
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected())
-            return true;
-        else
-            return false;
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 
     public String GET(String url){
@@ -103,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 GlobalJogo.resultadosPendentes = matches.getMatches();
 //                recyclerView.setVisibility(View.VISIBLE);
 //                progress.setVisibility(View.GONE);
-                adapter.notifyDataSetChanged();
+//                adapter.notifyDataSetChanged();
 
             } else
                 stringJson = "Did not work!";
@@ -115,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         return stringJson;
     }
 
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
         BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
         String line = "";
         String result = "";
@@ -133,4 +99,5 @@ public class MainActivity extends AppCompatActivity {
             return GET(urls[0]);
         }
     }
+
 }
